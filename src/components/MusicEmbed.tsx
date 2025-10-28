c70a85694c0ede2e4a0d8f282866e0cda6c64b71
guthierres <54993324+guthierres@users.noreply.github.com>
2025-10-28 15:56:50 -0300

    Update MusicEmbed.tsx

--- src/components/MusicEmbed.tsx
+++ src/components/MusicEmbed.tsx
@@ -78,13 +78,14 @@ export const MusicEmbed: React.FC<MusicEmbedProps> = ({ embed, onRemove, editabl
             </div>
           )}
           <iframe
-            src={`https://open.spotify.com/embed/${spotifyType}/${spotifyId}?utm_source=generator&theme=0`}
+            src={`https://open.spotify.com/embed/${spotifyType}/${spotifyId}?utm_source=generator`}
             width="100%"
             height="152"
             frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            allow="encrypted-media"
             loading="lazy"
             className="w-full"
+            style={{ border: 'none' }}
           />
         </div>
       </div>
